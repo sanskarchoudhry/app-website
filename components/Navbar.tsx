dde9579949b5020ce7b00679506a1eae1a95564a
sanskarchoudhry <sanskar22101@gmail.com>
2024-10-23 00:37:53 +0530

    navbar editing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,15 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { MenuItems, menuItems } from "../public/json_data";
 import { FaMessage, FaBell, FaHouse, FaComments } from "react-icons/fa6";
-import { FaRandom } from "react-icons/fa";
+import {
+  FaDiscord,
+  FaFacebook,
+  FaInstagram,
+  FaRandom,
+  FaTwitter,
+} from "react-icons/fa";
 import Image from "next/image";
+import SearchBox from "./SearchBox";
 
 function Navbar() {
   const [showProfileDropDown, setShowProfileDropdown] =
@@ -13,38 +20,65 @@ function Navbar() {
     useState<boolean>(false);
 
   return (
-    <nav className=" flex backdrop-blur-sm w-full items-center justify-evenly fixed top-0 z-50 py-4">
+    <nav className=" flex backdrop-blur-sm w-full items-center justify-between fixed top-0 z-50 py-4 px-14">
       <div className=" text-32 font-archivoBlack-regular">RECOMMENDER</div>
+      <div className="flex items-center gap-24">
+        <SearchBox />
+        <div className="text-24 font-satoshi-bold pl-4 flex gap-8 items-center">
+          <div className=" flex flex-col justify-center items-center leading-20">
+            <span className=" block text-20 font-satoshi-regular text-grey-secondary">
+              Join
+            </span>
+            <span className="block text-20 font-satoshi-regular text-grey-secondary">
+              Us
+            </span>
+          </div>
+          <ul className="flex gap-8 items-center cursor-pointer">
+            <li className=" h-[3rem] w-[3rem] rounded-full bg-bg-secondary flex items-center justify-center">
+              <FaDiscord />
+            </li>
+            <li className=" h-[3rem] w-[3rem] rounded-full bg-bg-secondary flex items-center justify-center">
+              <FaTwitter />
+            </li>
+            <li className=" h-[3rem] w-[3rem] rounded-full bg-bg-secondary flex items-center justify-center">
+              <FaFacebook />
+            </li>
+            <li className=" h-[3rem] w-[3rem] rounded-full bg-bg-secondary flex items-center justify-center">
+              <FaInstagram />
+            </li>
+          </ul>
+        </div>
+        <ul className=" flex gap-16">
+          <li className=" flex gap-4 flex-col items-center text-20 text-light-blue-primary">
+            <FaHouse />
+            <Link
+              href={""}
+              className="text-20 font-satoshi-regular hover:text-light-blue-primary text-white-primary"
+            >
+              Home
+            </Link>
+          </li>
+          <li className=" flex gap-4 flex-col items-center text-20 text-light-blue-primary">
+            <FaRandom />
+            <Link
+              href={"/movie/movie_id"}
+              className="text-20 font-satoshi-regular hover:text-light-blue-primary text-white-primary"
+            >
+              Random
+            </Link>
+          </li>
+          <li className=" flex gap-4 flex-col items-center text-20 text-light-blue-primary">
+            <FaComments />
+            <Link
+              href={"/community/posts"}
+              className="text-20 font-satoshi-regular hover:text-light-blue-primary text-white-primary"
+            >
+              Community
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-      <ul className=" flex gap-24">
-        <li className=" flex gap-4 flex-col items-center text-20 text-light-blue-primary">
-          <FaHouse />
-          <Link
-            href={""}
-            className="text-20 font-satoshi-regular hover:text-light-blue-primary text-white-primary"
-          >
-            Home
-          </Link>
-        </li>
-        <li className=" flex gap-4 flex-col items-center text-20 text-light-blue-primary">
-          <FaRandom />
-          <Link
-            href={"/movie/movie_id"}
-            className="text-20 font-satoshi-regular hover:text-light-blue-primary text-white-primary"
-          >
-            Random
-          </Link>
-        </li>
-        <li className=" flex gap-4 flex-col items-center text-20 text-light-blue-primary">
-          <FaComments />
-          <Link
-            href={"/community/posts"}
-            className="text-20 font-satoshi-regular hover:text-light-blue-primary text-white-primary"
-          >
-            Community
-          </Link>
-        </li>
-      </ul>
       <div>
         {/* <button className=" bg-button-primary-blue px-4 py-2 hover:bg-button-secondary-blue text-20 font-satoshi-bold hover:scale-105 active:scale-100 transition-transform">
           Log in
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,8 +1,24 @@
+import Link from "next/link";
+import { ChangeEvent, useState } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
 export default function SearchBox() {
+  const [searchText, setSearchText] = useState<string>("");
   return (
-    <section className=" w-[132rem] bg-pink-primary">
-      <input type="text" className="" />
-      <button className=" bg-">Search</button>
+    <section className=" flex h-10">
+      <input
+        type="text"
+        className=" outline-none text-grey-primary placeholder:text-grey-secondary p-2 text-20"
+        placeholder="Search"
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          setSearchText(e.target.value);
+        }}
+      />
+      <Link href={`/movie/${searchText}`}>
+        <button className=" bg-bg-secondary w-10 h-full text-24 flex items-center justify-center">
+          <FaMagnifyingGlass />
+        </button>
+      </Link>
     </section>
   );
 }
